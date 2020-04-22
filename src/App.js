3fad6835cc1d5acf139266ecb2432a8b623f1f56
victor.medeiros <victorbraga.8@hotmail.com>
2020-04-22 18:52:58 -0300

    Commit Inicial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Routes from './routes';
+
+// Isso pode ser feito por ser Stateless, retorna sem precisar de estado e só ter um Retrn puro
+const App = ()=> <Routes/> 
+
+// function App() {
+//   return (
+//     <Main/>
+//   );
+// }
+
+export default App;
